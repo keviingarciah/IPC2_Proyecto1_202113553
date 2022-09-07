b79b15d5582196d6db5091ca8b02e5af71c5d712
Kevin García <3737790890101@ingenieria.usac.edu.gt>
2022-09-06 23:08:53 -0600

    Final Commit

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, ttk, messagebox
-import load, simulation, diagnostic
+import load, simulation, diagnostic, webbrowser
 from object import Pacient
-
 
 
 class Menu(tk.Tk):
@@ -11,7 +10,7 @@ class Menu(tk.Tk):
         super().__init__()
 
         self.title("¡Bienvenido!")
-        self.geometry("430x380+600+170")
+        self.geometry("430x420+600+170")
         self.resizable(0,0)
 
         self.frame_1 = tk.Frame(self,width=240, height = 480)
@@ -32,8 +31,11 @@ class Menu(tk.Tk):
         self.button_2_2 = tk.Button(self.frame_2, text="Iniciar Simulación", command = self.simulate)
         self.button_2_2.grid(row=2, column=0, pady=10, padx=20)
 
+        self.button_2_2 = tk.Button(self.frame_2, text="Generar XML", command = self.xml)
+        self.button_2_2.grid(row=3, column=0, pady=10, padx=20)
+
         self.button_2_4 = tk.Button(self.frame_2, text="Salir", command = self.quit)
-        self.button_2_4.grid(row=3, column=0, pady=10, padx=20)
+        self.button_2_4.grid(row=4, column=0, pady=10, padx=20)
 
     def openFile(self):
         
@@ -50,6 +52,9 @@ class Menu(tk.Tk):
 
     def simulate(self):
         Simulation(self)
+
+    def xml(self):
+        XML(self)    
         
     def exit(self):  
         self.quit()    
@@ -63,7 +68,67 @@ class Simulation(tk.Toplevel):
         self.master = master
 
         self.title("Simular")
-        self.geometry("400x300+600+210")
+        self.geometry("400x270+610+230")
+        self.resizable(0,0)
+        self.protocol("WM_DELETE_WINDOW", self.back_to_main)
+
+        self.frame_o2 = tk.Frame(master=self,width=240, height = 480)
+        self.frame_o2.grid(row=0, column=0, padx=(30,30), pady=(30,15))
+
+        self.label_o_2 = tk.Label(master=self.frame_o2, text="Elija alguno: ", font=("Arial", 12))  # font name and size in px
+        self.label_o_2.grid(row=0, column=0, pady=(15,20), padx=10)
+
+        n = tk.StringVar()
+        self.combox = ttk.Combobox(master=self.frame_o2, width = 27, textvariable = n) 
+        self.combox.grid(row=0, column=1, pady=(15,20), padx=10)
+        
+        list = []
+
+        self.combox['values'] = Pacient.getPacient(list)
+
+        self.button_o_1 = tk.Button(master=self.frame_o2, text="Simular", command=self.simulate)
+        self.button_o_1.grid(row=1, column=1, pady=(10,20), padx=10)
+
+        self.button_o_3 = tk.Button(master=self, text="Regresar", command=self.back_to_main)
+        self.button_o_3.grid(row=2, column=0, pady=(0,20), padx=10)
+
+        self.master.withdraw()
+
+    def back_to_main(self):
+        self.master.deiconify()
+        self.destroy()   
+
+    def simulate(self):
+
+        print("\n Paciente: ", self.combox.get())
+        print("----------------------")
+        pacient = self.combox.get()
+        
+        results = simulation.simulate(pacient)  
+
+        if results[2] != 0:
+            messagebox.showinfo(message="El paciente tiene una "+str(results[0])+", donde el patrón se repite por primera vez" 
+            +"en el periodo "+str(results[1])+" cada "+str(results[2])+" periodos." , title="Simulación terminada.") 
+
+        elif results[1] != 0:    
+            messagebox.showinfo(message="El paciente tiene una "+str(results[0])+", donde el patrón se repite cada " 
+            +str(results[1])+" periodos." , title="Simulación terminada.") 
+
+        else:
+            messagebox.showinfo(message="El paciente tiene una "+str(results[0])+", por lo cual podrá vivir una bonita vida :D.", title="Simulación terminada.") 
+
+        messagebox.showinfo(message="Se generaron cada uno de los periodos de la simulación.", title="Rejillas generadas.") 
+        webbrowser.open('Gráficas')
+
+class XML(tk.Toplevel):
+
+    def __init__(self, master):
+        super().__init__()
+        
+        self.master = master
+
+        self.title("Generar XML")
+        self.geometry("400x270+610+240")
         self.resizable(0,0)
         self.protocol("WM_DELETE_WINDOW", self.back_to_main)
 
@@ -82,35 +147,27 @@ class Simulation(tk.Toplevel):
 
         self.combox['values'] = Pacient.getPacient(list)
 
-        self.button_o_1 = tk.Button(master=self.frame_o2, text="Simular", command=self.simulate)
+        self.button_o_1 = tk.Button(master=self.frame_o2, text="Generar XML", command=self.generate)
         self.button_o_1.grid(row=1, column=1, pady=(10,20), padx=10)
 
-        self.button_o_2 = tk.Button(master=self, text="Generar XML", command=self.XML)
-        self.button_o_2.grid(row=2, column=0, pady=(15,20), padx=10)
-
         self.button_o_3 = tk.Button(master=self, text="Regresar", command=self.back_to_main)
-        self.button_o_3.grid(row=3, column=0, pady=(0,20), padx=10)
+        self.button_o_3.grid(row=2, column=0, pady=(0,20), padx=10)
 
         self.master.withdraw()
+
 
     def back_to_main(self):
         self.master.deiconify()
         self.destroy()   
 
-    def simulate(self):
+    def generate(self):
 
-        print(self.combox.get())
+        print("\nSe da el XML de ",self.combox.get())
         pacient = self.combox.get()
-        
-        simulation.simulate(pacient)  
-        messagebox.showinfo(message="Se realizó la simulación y se generaron los gráficos.", title="Simulación terminada.") 
 
-    def XML(self):
-
-        pacient = self.combox.get()
-        print("Se da el xml") 
         diagnostic.generateXML(pacient)   
-        messagebox.showinfo(message="Se generó el archivo XML.", title="Resultados cargados")  
+        messagebox.showinfo(message="Se generó el archivo XML.", title="Resultados cargados")   
+
 
 if __name__ == "__main__":
 
--- src/object.py
+++ src/object.py
@@ -10,8 +10,6 @@ class Pacient:
         self.age = age
         self.periods = periods
         self.m = m
-        #self.c = c
-        #self.f = f
         self.results = results
         self.n = n
         self.n1 = n1
@@ -31,19 +29,11 @@ class Pacient:
     def add_to_list(name, age, periods, m, results, n, n1, cells):
 
         pacients.append(Pacient(name, age, periods, m, results, n, n1, cells))
-        #print(pacients)
-        #print(len(pacients))
-
-        #for i in pacients:
-            #print(i.name, i.age, i.periods, i.m)
-            #print(i.cells)
 
 
     def getPacient(values):
-        #print("Se obtienen el paciente: ", len(pacients))
 
         for i in pacients:
-            #print(i.name)
             z = i.name
             values.append(z)
 
@@ -51,33 +41,27 @@ class Pacient:
 
 
     def getDimension(pacient):
-        #print("Se obtienen las dimensiones: ")
 
         for i in pacients:
             if i.name == pacient:
-                #print(i.m)
                 x = i.m
                 break
 
         return x    
 
     def getCells(pacient):
-        #print("Se obtienen las coordenadas: ", pacient)
 
         for i in pacients:
             if i.name == pacient:
-                #print(i.cells)
                 y = i.cells
                 break
 
         return y      
 
     def getPeriods(pacient):
-        #print("Se obtienen los periodos: ", pacient)
 
         for i in pacients:
             if i.name == pacient:
-                #print(i.periods)
                 n = i.periods
                 break
 
@@ -85,7 +69,6 @@ class Pacient:
 
 
     def editResults(pacient, results, n0, n1):
-        #print("Se editan los resultados de: ", pacient)
 
         for i in pacients:
 
@@ -95,21 +78,11 @@ class Pacient:
                 i.n = n0
                 i.n1 = n1
 
-                #print(i.results, i.n, i.n1)
-
                 break
      
     def getResults (all):
 
         for i in pacients:
-
-            name = i.name 
-            age = i.age
-            periods = i.periods
-            m = i.m
-            results = i.results
-            n = i.n
-            n1 = i.n1
 
             data = [i.name, i.age, i.periods, i.m, i.results, i.n, i.n1]
 
@@ -137,22 +110,12 @@ class Grids:
 
         infected.append(Grids(period, coords, n0, n1, pacient))
 
-        #print("Se agrego la rejilla")
-        #print(infected)
-        #print(len(infected))
-
-       #for i in infected:
-       #    print(i.period, i.pacient)
-       #    print(i.coords)
-
     def getGrid(k):
-        #print("Se obtienen la rejilla: ", k)
 
         for i in infected:
 
             if i.period == k:
-                #print("Las coordenadas: ")
-                #print(i.coords)
+
                 ords = i.coords
 
                 return ords
@@ -162,61 +125,41 @@ class Grids:
 
         status = ""
 
-        #print("Comparando periodo: ", n, "de paciente : ", pacient)
-        #print(grid, "\n")
-
         for i in infected:
 
             if grid == i.coords:
-
-                #print("Las coordenadas se repiten en: ", i.period)
-                #print(i.coords, "\n")
-
                 period_repeated =  i.period
 
                 if period_repeated == 0:
-                    
                     n0 = n - period_repeated
                     n1 = 0
 
-                    #print("NO. DE PERIODOS: ", n0, "\n")
-                
-
                     if n0 == 1:
-                        #print("Enfermedad MORTAL")
-
                         status = "Enfermedad MORTAL"
                         return status, n0, n1 
 
                     else:
-                        #print("Enfermedad GRAVE")   
+              
                         status = "Enfermedad GRAVE"
-                        #return status
+
 
                 else: 
-                    
-
                     n0 = n
                     n1 = n - period_repeated
 
-                    #print("NO. DE PERIODOS: ", n1, "\n")
-
                     if n1 == 1:
-                        #print("Enfermedad MORTAL")
                         status = "Enfermedad MORTAL"
                         return status, n0, n1 
 
 
                     else:
-                        #print("Enfermedad GRAVE")   
                         status = "Enfermedad GRAVE"
-                        #return status
+
 
         if status == "Enfermedad GRAVE":
             return status, n0, n1 
 
         else:    
-            #print("ENFERMEDAD LEVE")   
             status = "Enfermedad LEVE"
             n0 = 0
             n1 = 0
